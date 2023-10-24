ae68334d9426ddc61aa954429c0e2dcc785dad2d
Camille Gautier <camille.gaut@sfr.fr>
2023-10-24 21:50:52 +0200

    [FEAT] [CGA] fix database connection

--- src/database/postgres.js
+++ src/database/postgres.js
@@ -1,27 +1,27 @@
-const { Sequelize, DataTypes } = require("sequelize");
-const UserModel = require("../models/user.model");
-const bcrypt = require("bcrypt");
+const { Sequelize, DataTypes } = require('sequelize');
+const UserModel = require('../models/user.model');
+const bcrypt = require('bcrypt');
 
 let sequelize;
 
-if (process.env.NODE_ENV === "production") {
+if (process.env.NODE_ENV === 'production') {
    sequelize = new Sequelize(
-      "db8s2lm3nc0hp9",
-      "mssjrtppzqoxku",
-      "c0f472454343fb8f041d4f5fb60c0b1dcd10c5f5d04e6141b04186c797b82048",
+      'db8s2lm3nc0hp9',
+      'mssjrtppzqoxku',
+      'c0f472454343fb8f041d4f5fb60c0b1dcd10c5f5d04e6141b04186c797b82048',
       {
-         host: "ec2-54-84-182-168.compute-1.amazonaws.com",
+         host: 'ec2-54-84-182-168.compute-1.amazonaws.com',
          dialect:
-            "postgres" /* one of 'mysql' | 'postgres' | 'sqlite' | 'mariadb' | 'mssql' | 'db2' | 'snowflake' | 'oracle' */,
+            'postgres' /* one of 'mysql' | 'postgres' | 'sqlite' | 'mariadb' | 'mssql' | 'db2' | 'snowflake' | 'oracle' */,
          logging: true,
       }
    );
 } else {
    //Passing parameters separately (other dialects)
-   sequelize = new Sequelize("userapipokemons", "postgres", "root", {
-      host: "localhost",
+   sequelize = new Sequelize('userapipokemons', 'postgres', 'root', {
+      host: 'localhost',
       dialect:
-         "postgres" /* one of 'mysql' | 'postgres' | 'sqlite' | 'mariadb' | 'mssql' | 'db2' | 'snowflake' | 'oracle' */,
+         'postgres' /* one of 'mysql' | 'postgres' | 'sqlite' | 'mariadb' | 'mssql' | 'db2' | 'snowflake' | 'oracle' */,
       logging: false,
    });
 }
@@ -31,19 +31,19 @@ const User = UserModel(sequelize, DataTypes);
 sequelize
    .authenticate()
    .then((_) =>
-      console.log("Connection has been established successfully. postgres")
+      console.log('Connection has been established successfully. postgres')
    )
    .catch((error) => {
-      console.error("Unable to connect to the database:", error);
+      console.error('Unable to connect to the database:', error);
    });
 
 const initDB = () => {
    return sequelize.sync({ force: true }).then((_) => {
-      console.log("Init user");
-      const myPlaintextPassword = "camsaka";
+      console.log('Init user');
+      const myPlaintextPassword = 'camsaka';
       bcrypt.hash(myPlaintextPassword, 5).then((hash) => {
          User.create({
-            username: "camsaka",
+            username: 'camsaka',
             password: hash,
          }).then((user) => console.log(user));
       });
